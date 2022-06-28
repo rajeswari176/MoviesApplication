b7523e6d66e071de694c050d265d35c3a2870cc4
rajeswari176 <rajeswari176.bits@gmail.com>
2022-06-28 17:13:16 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -4,45 +4,19 @@ let log = document.getElementById('values');
 let totalResults=0;
 let timer;
 
-
-// function getMovies(){
-//         fetch("http://www.omdbapi.com/?apikey=b9bd48a6&s=Marvel&type=movie")
-//         .then(response => {
-//             return response.json();
-//         })
-//         .then(data => {
-//             console.log("movies", data);
-//             return data;    
-//         })
-// }
-// const movies = getMovies();
-
-
 const URL = 'http://api.adorable.io/avatars/'
 
 function getRandNum(){
     return Math.floor(Math.random() * 100)
 
 }
-var counter = 0;
+var counter = 1;
 
-// function updateValue(e) {
-//     log.textContent = e.target.value;
-//     loadImages();
-//   }
 function loadImages(searchval,pagenum){
-    if(counter < totalResults)
-    // let i = 0;
-    // while(i < numImages){
-    //     const img = document.createElement('img')
-    //     // img.src = `${URL}${getRandNum()}`
-    //     img.src = "https://picsum.photos/200"
-    //     container.appendChild(img)
-    //     i++
-    //     counter++;
-    // }
-    const url = "http://www.omdbapi.com/?apikey=b9bd48a6&type=movie&s="+searchval+"&page="+pagenum
-    
+    if(pagenum <= 18) {
+    console.log("pagenum", pagenum)
+    var url = "http://www.omdbapi.com/?apikey=b9bd48a6&type=movie&s="+searchval+"&page="+pagenum
+    counter++;
 
     fetch(url)
     .then(response => {
@@ -51,28 +25,18 @@ function loadImages(searchval,pagenum){
     .then(data => {
         console.log("movies", data);
         totalResults = data.totalResults;
-        for(var i=counter; i<10; i++){
+        for(var i=0; i<10; i++){
             console.log("i val", i)
             const img = document.createElement('img')
             img.src = data.Search[i].Poster
             container.appendChild(img)
-            counter++;
 
         }
         console.log("finished the loop")
         return data;    
     })
-
-    // let i=0;
-    // while(i<numImages){
-    //          const img = document.createElement('img')
-    //     img.src = movies.Search(i).Poster
-    //     container.appendChild(img)
-    //     i++
-
-    // }
 }
-//console.log("serach...", search);
+}
 input.addEventListener('input', function(e){
     clearTimeout(timer);
     log.textContent = e.target.value;
@@ -81,9 +45,8 @@ input.addEventListener('input', function(e){
     }, 1000);
 });
 window.addEventListener('scroll', () =>{
-  //  console.log(window.innerHeight,"a", window.scrollY,"b",document.documentElement.scrollHeight)
     if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight && counter < 60){
-       loadImages(log.textContent,)
+       loadImages(log.textContent,counter)
        console.log("inside if")
     }
 })
