2a39d6ce75adf31a5c3b33e56bb4bbd1df0d8a5f
rajeswari176 <rajeswari176.bits@gmail.com>
2022-06-28 17:16:09 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -3,40 +3,34 @@ let input = document.querySelector('input');
 let log = document.getElementById('values');
 let totalResults=0;
 let timer;
-
-const URL = 'http://api.adorable.io/avatars/'
-
-function getRandNum(){
-    return Math.floor(Math.random() * 100)
-
-}
 var counter = 1;
 
 function loadImages(searchval,pagenum){
     if(pagenum <= 18) {
-    console.log("pagenum", pagenum)
-    var url = "http://www.omdbapi.com/?apikey=b9bd48a6&type=movie&s="+searchval+"&page="+pagenum
-    counter++;
+        console.log("pagenum", pagenum)
+        var url = "http://www.omdbapi.com/?apikey=b9bd48a6&type=movie&s="+searchval+"&page="+pagenum
+        counter++;
 
-    fetch(url)
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log("movies", data);
-        totalResults = data.totalResults;
-        for(var i=0; i<10; i++){
-            console.log("i val", i)
-            const img = document.createElement('img')
-            img.src = data.Search[i].Poster
-            container.appendChild(img)
+        fetch(url)
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            console.log("movies", data);
+            totalResults = data.totalResults;
+            for(var i=0; i<10; i++){
+                console.log("i val", i)
+                const img = document.createElement('img')
+                img.src = data.Search[i].Poster
+                container.appendChild(img)
 
-        }
-        console.log("finished the loop")
-        return data;    
-    })
+            }
+            console.log("finished the loop")
+            return data;    
+        })
+    }
 }
-}
+
 input.addEventListener('input', function(e){
     clearTimeout(timer);
     log.textContent = e.target.value;
@@ -44,6 +38,7 @@ input.addEventListener('input', function(e){
         loadImages(log.textContent,1);
     }, 1000);
 });
+
 window.addEventListener('scroll', () =>{
     if(window.scrollY + window.innerHeight >= document.documentElement.scrollHeight && counter < 60){
        loadImages(log.textContent,counter)
